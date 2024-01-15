492f9006dbe1ba0b66766a09bace7a4c992fb42c
jessica1519 <marcianojessica90@gmail.com>
2024-01-15 15:45:21 +0100

    Deploy portfolio  app to GitHub Pages

---
+++ precache-manifest.0ed4daeb1a92ca1e949d5a5b9a5fc19a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86e442dc3eec3de5f8bf2305a5fb981d",
+    "url": "/portfolio-sites/index.html"
+  },
+  {
+    "revision": "a652d74b44ff52f429bc",
+    "url": "/portfolio-sites/static/css/main.4de458fc.chunk.css"
+  },
+  {
+    "revision": "1f6bf91d57232062b07f",
+    "url": "/portfolio-sites/static/js/2.f153d16f.chunk.js"
+  },
+  {
+    "revision": "dedb3723e98c200d965bfe8c0aef3544",
+    "url": "/portfolio-sites/static/js/2.f153d16f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a652d74b44ff52f429bc",
+    "url": "/portfolio-sites/static/js/main.f2602dd5.chunk.js"
+  },
+  {
+    "revision": "6676ed2086577f2fd9c1",
+    "url": "/portfolio-sites/static/js/runtime-main.45243ef9.js"
+  },
+  {
+    "revision": "07a1b0167c00e41af6a442e2497aa7f8",
+    "url": "/portfolio-sites/static/media/investimenti.07a1b016.png"
+  },
+  {
+    "revision": "68c776586185375d0bd098d0d3dd8b4c",
+    "url": "/portfolio-sites/static/media/motivazionale.68c77658.png"
+  },
+  {
+    "revision": "ec9d8351149be597fdbc2feb8b15471a",
+    "url": "/portfolio-sites/static/media/qrCode.ec9d8351.png"
+  },
+  {
+    "revision": "359702ba7a2fd583320a109c6654fe56",
+    "url": "/portfolio-sites/static/media/sitoImgFoto.359702ba.png"
+  },
+  {
+    "revision": "749f5c7d3e99cd4a52f9f3777bb2bc0e",
+    "url": "/portfolio-sites/static/media/woman.749f5c7d.png"
+  }
+]);
